26cd9cfddc8b647aad6ca21fec5494a186bc70b6
Ever <evermedina2025@gmail.com>
2021-10-24 19:01:16 -0700

    Navbar links and styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,24 +2,35 @@ import React from "react"
 import App from "../App";
 import logo from "./logo.png"
 import css from "../App.css"
+// fontawesome imports
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export const Navbar = () => {
     return (
 <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+  <div className = "container">
   <a class="navbar-brand" href="#"><img className="logo" src={logo} alt="logo..."/></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+    <FontAwesomeIcon icon={faBars} style={{color: "#fff"}}/>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav ml-auto">
+    <ul class="navbar-nav">
       <li class="nav-item active">
         <a class="nav-link" href="#">Home</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#">about Me</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">portfolio</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">contacts</a>
       </li>
     </ul>
+  </div>
   </div>
 </nav>
     )
