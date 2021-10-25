d5c91ed11696cdfc29fe50f2878084580e4fd5d0
Ever <evermedina2025@gmail.com>
2021-10-24 20:54:01 -0700

    Completed Header and background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
+import Header from  "./components/Header";
+import Particles from "react-particles-js";
 
 function App() {
   return (
+    <>
    <Navbar/>
+   <Header />
+   </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Typed from "react-typed";
+
+const Header = () => {
+    return (
+       <div className="header-wrapper">
+         <div className="main-info">
+            <h1>Web Development</h1>
+            <Typed
+            className="typed-text"
+            strings={["Web Design", "Web Development",]}
+            typeSpeed={60}
+            backspeed={60}
+            loop
+            />
+            <a href="#" className="btn-main-offer">contact me</a>
+        </div>
+       </div>
+    )
+}
+
+export default Header;
